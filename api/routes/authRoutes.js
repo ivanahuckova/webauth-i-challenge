da7e654f1e94a0a6a6cbcaa5aa2b0afd57076385
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-01 14:00:31 +0200

    Initial set up of server and routes

---
+++ api/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const knex = require('knex');
+
+//WIP
+//const knexConfig = require(knex).development
+//const db=kneex(knexConfig)
+const routes = express.Router();
+
+module.exports = routes;
