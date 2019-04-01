0a376191bf2f110999659b665355d08f851cf6b2
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-01 15:09:46 +0200

    Set up login post route

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -11,6 +11,7 @@ routes.get('/', (req, res) => {
   res.json('It is working');
 });
 
+//REGISTER ROUTE
 routes.post('/api/register', async (req, res) => {
   const user = req.body;
   try {
@@ -18,7 +19,29 @@ routes.post('/api/register', async (req, res) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       user.password = hashedPassword;
       const newUser = await db('users').insert(user);
-      res.status(201).json(newUser);
+      res.status(201).json({ id: newUser[0], user: user.username, password: user.password });
+    } else {
+      res.status(400).json({ message: 'You need to include username and password' });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//LOGIN ROUTE
+routes.post('/api/login', async (req, res) => {
+  const user = req.body;
+  try {
+    if (user.password && user.username) {
+      const specificUser = await db('users')
+        .where({ username: user.username })
+        .first();
+      const specificUserPassword = await bcrypt.compareSync(user.password, specificUser.password);
+      if (specificUser && specificUserPassword) {
+        res.status(200).json(`${specificUser.username} is logged in`);
+      } else {
+        res.status(400).json({ message: 'Login credential does not work' });
+      }
     } else {
       res.status(400).json({ message: 'You need to include username and password' });
     }
